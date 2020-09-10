860116f636b7a0c6b8bc35dd0c7343af8c6b55da
Rishabh Nevatia <nevatia.rishabh@gmail.com>
2020-09-11 02:04:50 +0530

    Comment creation and styling

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var Campground = require('./models/campground');
+var Comment = require('./models/comment');
 var seedDB = require("./seeds");
 
 seedDB();
@@ -14,6 +15,7 @@ mongoose.connect("mongodb://localhost:27017/yelp_camp", {
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
 
 app.get('/', function(req, res){
     res.render('landing');
@@ -24,7 +26,7 @@ app.get('/campgrounds', function(req, res){
         if(err){
             console.log(err);
         } else{
-            res.render('index', {campgrounds: allCampgrounds});
+            res.render('campgrounds/index', {campgrounds: allCampgrounds});
         }
     });
 });
@@ -45,7 +47,7 @@ app.post('/campgrounds', function(req, res){
 });
 
 app.get('/campgrounds/new', function(req, res){
-    res.render('new');
+    res.render('campgrounds/new');
 })
 
 app.get('/campgrounds/:id', function(req, res){
@@ -53,7 +55,38 @@ app.get('/campgrounds/:id', function(req, res){
         if(err){
             console.log(err);
         } else{
-            res.render("show", {campground: foundCampground});
+            res.render("campgrounds/show", {campground: foundCampground});
+        }
+    });
+});
+
+// COMMENTS ROUTES
+
+app.get('/campgrounds/:id/comments/new', function(req, res){
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        } else {
+            res.render('comments/new',{campground: campground});
+        }
+    });
+});
+
+app.post('/campgrounds/:id/comments', function(req, res){
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+            res.redirect('/campgrounds');
+        } else {
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err);
+                } else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect('/campgrounds/'+campground._id);
+                }
+            });
         }
     });
 });
